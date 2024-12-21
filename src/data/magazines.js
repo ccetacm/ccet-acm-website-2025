f0b70a38b2b4f13b416c9c0cbde45ca28582d9f1
HarshitVashisht11 <harshitvashisht07@gmail.com>
2024-12-21 10:25:31 +0530

    fix

---
+++ src/data/magazines.js
@@ -0,0 +1,70 @@
+import janFebCover from "../acm-assets-2/jan-feb-cover.jpg"
+import marAprCover from "../acm-assets-2/march-april-cover.jpg"
+import mayJunCover from "../acm-assets-2/may-june-cover.jpg"
+import sepOctCover from "../acm-assets-2/sep-oct-cover.jpg"
+import novDecCover from "../acm-assets-2/nov-dec-cover.jpg"
+import julAugCover from "../acm-assets-2/jul-aug-cover.jpg";
+
+export const magazineData = [
+  {
+    id: 'v5i1',
+    volume: 5,
+    issue: 1,
+    title: 'January-February 2024',
+    description: '',
+    thumbnail: janFebCover,
+    pdfLink: "https://drive.google.com/file/d/1RVxUc6YCNxRl7jDBgNtmfqbLdadHG4D6/view?usp=drivesdk",
+    keywords: ['blockchain', 'ai', 'technology', 'innovation','machine learning','web3']
+  },
+  {
+    id: 'v5i2',
+    volume: 5,
+    issue: 2,
+    title: 'March-April 2024',
+    description: '',
+    thumbnail: marAprCover,
+    pdfLink: "https://drive.google.com/file/d/1ALPaqVdvQNVLjRtiTEuX2_QV3LCnt8pS/view?usp=drivesdk",
+    keywords: ['ai', 'machine learning', 'data science', 'technology','web3']
+  },
+  {
+    id: 'v5i3',
+    volume: 5,
+    issue: 3,
+    title: 'May-June 2024',
+    description: '',
+    thumbnail: mayJunCover,
+    pdfLink: "https://drive.google.com/file/d/1dWspZPHLQ42l8PE99ekXiWxpuBMeiBaq/view?usp=drivesdk",
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v5i4',
+    volume: 5,
+    issue: 4,
+    title: 'July-August 2024',
+    description: '',
+    thumbnail: julAugCover,
+    pdfLink: "https://drive.google.com/file/d/1lVH9IXeG0teEukrsVxr7qgYYcNG_BLEx/view?usp=drivesdk",
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v5i5',
+    volume: 5,
+    issue: 5,
+    title: 'September-October 2024',
+    description: '',
+    thumbnail: sepOctCover,
+    pdfLink: "https://drive.google.com/file/d/12V4lpS0TRFNNtmO-zfCG5-6nv3jJwPol/view?usp=drivesdk",
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v5i6',
+    volume: 5,
+    issue: 6,
+    title: 'November-December 2024',
+    description: '',
+    thumbnail: novDecCover,
+    pdfLink: "https://drive.google.com/file/d/1vBhfkqrb36AIEZt2ME5584IG96JQCphY/view?usp=drivesdk",
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  // ... (previous data with keywords)
+];
