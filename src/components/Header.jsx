b18849e2ae368d912ce3129c084a8a8892846f3f
Japjot Singh Nanda <japjotwork007@gmail.com>
2025-01-06 15:12:27 +0530

    solving issue#17

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,26 +22,31 @@ const Header = () => {
 
   const handleClick = () => {
     if (!openNavigation) return;
-
     enablePageScroll();
     setOpenNavigation(false);
   };
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center px-4 lg:px-6 xl:px-8 max-lg:py-4">
+      <div className="relative flex items-center px-4 lg:px-6 xl:px-8 max-lg:py-4">
         <a className="block w-[10rem] xl:mr-4" href="/">
-          <img src={"./acm-assets-2/ccet-logo-white.png"} width={150} height={35} alt="Acm" />
+          <img 
+            src={"./acm-assets-2/ccet-logo-white.png"} 
+            width={150} 
+            height={35} 
+            alt="Acm" 
+            className="max-w-full"
+          />
         </a>
 
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed inset-0 top-[5rem] bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
@@ -65,23 +70,25 @@ const Header = () => {
           <HamburgerMenu />
         </nav>
 
-        <a
-          href="#footer"
-          className="button hidden mr-6 text-n-1/50 transition-colors hover:text-n-1 lg:block"
-        >
-          Contact
-        </a>
-        <Button className="hidden lg:flex" href="/acmw">
-          ACM-W
-        </Button>
+        <div className="flex items-center ml-auto">
+          <a
+            href="#footer"
+            className="button hidden mr-6 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+          >
+            Contact
+          </a>
+          <Button className="hidden lg:flex" href="/acmw">
+            ACM-W
+          </Button>
 
-        <Button
-          className="ml-auto lg:hidden"
-          px="px-3"
-          onClick={toggleNavigation}
-        >
-          <MenuSvg openNavigation={openNavigation} />
-        </Button>
+          <Button
+            className="lg:hidden"
+            px="px-3"
+            onClick={toggleNavigation}
+          >
+            <MenuSvg openNavigation={openNavigation} />
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/data/magazines.js
+++ src/data/magazines.js
@@ -1,3 +1,46 @@
+import janFeb2024Cover from "../acm-assets-2/jan-feb-2024-cover.jpg"
+import janFeb2024Pdf from "../pdfs/jan-feb-2024.pdf"
+
+import marApr2024Cover from "../acm-assets-2/march-april-2024-cover.jpg"
+import marApr2024Pdf from "../pdfs/march-april-2024.pdf"
+
+import mayJun2024Cover from "../acm-assets-2/may-june-2024-cover.jpg";
+import mayJun2024Pdf from "../pdfs/may-jun-2024.pdf";
+
+import julAug2024Cover from "../acm-assets-2/jul-aug-2024-cover.jpg";
+import julAug2024Pdf from "../pdfs/jul-aug-2024.pdf";
+
+import sepOct2024Cover from "../acm-assets-2/sep-oct-2024-cover.jpg";
+import sepOct2024Pdf from "../pdfs/sep-oct-2024.pdf";
+
+import novDec2024Cover from "../acm-assets-2/nov-dec-2024-cover.jpg"
+import novDec2024Pdf from "../pdfs/nov-dec-2024.pdf"
+
+import aprMay2022Cover from "../acm-assets-2/April-May-2022-cover.jpg"
+import aprMar2022Pdf from "../pdfs/April-May-2022.pdf";
+
+import augSep2022Cover from "../acm-assets-2/August-Sep-2022-cover.jpg"
+import augSep2022Pdf from "../pdfs/August-Sep-2022.pdf";
+
+import janFeb2022Cover from "../acm-assets-2/jan-feb-2022-cover.jpeg"
+import janFeb2022Pdf from "../pdfs/Jan-Feb-2022.pdf"
+
+import julAug2021Cover from "../acm-assets-2/July-August-2021-cover.jpg"
+import julAug2021Pdf from "../pdfs/July-August-2021.pdf"
+
+import marApr2021Cover from "../acm-assets-2/March-April-2021-cover.jpg"
+import marApr2021Pdf from "../pdfs/March-April-2021.pdf"
+
+import mayJun2021Cover from "../acm-assets-2/May-June-2021-cover.jpg"
+import mayJun2021Pdf from "../pdfs/May-June-2021.pdf";
+
+import novDec2021Cover from "../acm-assets-2/nov-dec-2021-cover.jpeg"
+import novDec2021Pdf from "../pdfs/Nov-Dec-2021.pdf";
+
+import sepOct2021Cover from "../acm-assets-2/sep-oct-2021-cover.jpeg";
+import sepOct2021Pdf from "../pdfs/Sep-Oct-2021.pdf";
+
+
 export const magazineData = [
   {
     id: 'v5i1',
@@ -5,8 +48,8 @@ export const magazineData = [
     issue: 1,
     title: 'January-February 2024',
     description: '',
-    thumbnail: "./acm-assets-2/jan-feb-cover.jpg",
-    pdfLink: "./pdfs/jan-feb-pdf.pdf",
+    thumbnail: janFeb2024Cover,
+    pdfLink: janFeb2024Pdf,
     keywords: ['blockchain', 'ai', 'technology', 'innovation','machine learning','web3']
   },
   {
@@ -15,8 +58,8 @@ export const magazineData = [
     issue: 2,
     title: 'March-April 2024',
     description: '',
-    thumbnail: "./acm-assets-2/march-april-cover.jpg",
-    pdfLink: "./pdfs/mar-apr-pdf.pdf",
+    thumbnail: marApr2024Cover,
+    pdfLink: marApr2024Pdf,
     keywords: ['ai', 'machine learning', 'data science', 'technology','web3']
   },
   {
@@ -25,8 +68,8 @@ export const magazineData = [
     issue: 3,
     title: 'May-June 2024',
     description: '',
-    thumbnail: "./acm-assets-2/may-june-cover.jpg",
-    pdfLink: "./pdfs/may-jun-pdf.pdf",
+    thumbnail: mayJun2024Cover,
+    pdfLink: mayJun2024Pdf,
     keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
   },
   {
@@ -35,8 +78,8 @@ export const magazineData = [
     issue: 4,
     title: 'July-August 2024',
     description: '',
-    thumbnail: "./acm-assets-2/jul-aug-cover.jpg",
-    pdfLink: "./pdfs/jul-aug-pdf.pdf",
+    thumbnail: julAug2024Cover,
+    pdfLink: julAug2024Pdf,
     keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
   },
   {
@@ -45,8 +88,8 @@ export const magazineData = [
     issue: 5,
     title: 'September-October 2024',
     description: '',
-    thumbnail: "./acm-assets-2/sep-oct-cover.jpg",
-    pdfLink: "./pdfs/sep-oct-pdf.pdf",
+    thumbnail: sepOct2024Cover,
+    pdfLink: sepOct2024Pdf,
     keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
   },
   {
@@ -55,8 +98,88 @@ export const magazineData = [
     issue: 6,
     title: 'November-December 2024',
     description: '',
-    thumbnail: "./acm-assets-2/nov-dec-cover.jpg",
-    pdfLink: "./pdfs/nov-dec-pdf.pdf",
+    thumbnail: novDec2024Cover,
+    pdfLink: novDec2024Pdf,
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v2i4',
+    volume: 2,
+    issue: 4,
+    title: 'April-May 2022',
+    description: '',
+    thumbnail: aprMay2022Cover,
+    pdfLink: aprMar2022Pdf,
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v3i1',
+    volume: 3,
+    issue: 1,
+    title: 'August-September 2022',
+    description: '',
+    thumbnail: augSep2022Cover,
+    pdfLink: augSep2022Pdf,
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v3i2',
+    volume: 3,
+    issue: 2,
+    title: 'January-February 2022',
+    description: '',
+    thumbnail: janFeb2022Cover,
+    pdfLink: janFeb2022Pdf,
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v1i3',
+    volume: 1,
+    issue: 3,
+    title: 'July-August 2021',
+    description: '',
+    thumbnail: julAug2021Cover,
+    pdfLink: julAug2021Pdf,
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v1i1',
+    volume: 1,
+    issue: 1,
+    title: 'March-April 2021',
+    description: '',
+    thumbnail: marApr2021Cover,
+    pdfLink: marApr2021Pdf,
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v1i2',
+    volume: 1,
+    issue: 2,
+    title: 'May-June 2021',
+    description: '',
+    thumbnail: mayJun2021Cover,
+    pdfLink: mayJun2021Pdf,
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v2i2',
+    volume: 2,
+    issue: 2,
+    title: 'November-December 2021',
+    description: '',
+    thumbnail: novDec2021Cover,
+    pdfLink: novDec2021Pdf,
+    keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
+  },
+  {
+    id: 'v2i1',
+    volume: 2,
+    issue: 1,
+    title: 'September-October 2021',
+    description: '',
+    thumbnail: sepOct2021Cover,
+    pdfLink: sepOct2021Pdf,
     keywords: ['blockchain', 'crypto', 'decentralization', 'web3']
   },
 ];
